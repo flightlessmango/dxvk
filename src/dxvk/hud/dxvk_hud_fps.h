95138b8a78928f7b1e91138d755d3cbb14d3753f
FlightlessMango <flightlessmangoyt@gmail.com>
2019-09-20 18:22:02 +0200

    Less messy and windows friendly cpu usage fetching.

--- src/dxvk/hud/dxvk_hud_fps.h
+++ src/dxvk/hud/dxvk_hud_fps.h
@@ -36,9 +36,11 @@ namespace dxvk::hud {
     const HudElements m_elements;
     
     std::string m_fpsString;
+    std::string m_cpuUtilizationString;
     bool mango_logging = false;
     time_t lastPress = time(0);
-    char const* logging;
+    char const* logging = getenv("DXVK_LOG_TO_FILE");
+    int64_t fps;
     
     TimePoint m_prevFpsUpdate;
     TimePoint m_prevFtgUpdate;
